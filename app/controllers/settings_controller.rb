191d3dede80a49be2ef84f731af5380e52adadd0
Mehmet Samil <mehmet.samil.buyukcaglayan@lab2023.com>
2019-07-08 18:00:33 +0300

    turbolink test

--- app/controllers/settings_controller.rb
+++ app/controllers/settings_controller.rb
@@ -61,6 +61,10 @@ class SettingsController < ApplicationController
     end
   end
 
+  def render
+
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_setting
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :options
   resources :settings
+  get 'settings/render', to: 'settings#render', as: :render_form
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
